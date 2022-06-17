1662a846d17f7f0c5ec86cf7670033ca5ff6ca58
JMARyA <jmarya@hydrar.de>
2022-06-17 16:01:29 +0200

    better journal root dir handling

--- src/etc.rs
+++ src/etc.rs
@@ -2,13 +2,41 @@ use chrono::Datelike;
 use directories::UserDirs;
 use std::fs;
 use std::process::Command;
+use std::env::var;
+use std::path::Path;
+
+pub fn get_document_path() -> String {
+    // Check for environment variable
+    let doc = var("JOURNAL_DIR");
+    if doc.is_ok() {
+        return doc.ok().unwrap();
+    }
+
+    // Check for user document folder
+    let user_dirs = UserDirs::new();
+    let user_dirs = user_dirs.unwrap();
+    let doc = user_dirs.document_dir();
+
+    if doc.is_some() {
+        let doc = doc.unwrap();
+        return doc.to_str().unwrap().to_string();
+    }
+
+    // Check for XDG Config Dir
+    let doc = var("XDG_CONFIG_HOME");
+    if doc.is_ok() {
+        return doc.ok().unwrap();
+    }
+
+    // Manual XDG Config Dir
+    let home = var("HOME");
+    let doc = Path::new(&home.unwrap()).join(".config").to_str().unwrap().to_string();
+    return doc;
+}
 
 /// Get Root Journal Directory
 pub fn journal_dir() -> String {
-    let user_dirs = UserDirs::new();
-    let user_dirs = user_dirs.unwrap();
-    let docs = user_dirs.document_dir().unwrap();
-    return format!("{}/Journal", docs.to_str().unwrap());
+    return format!("{}/Journal", get_document_path());
 }
 
 /// Get current day journal path
@@ -36,8 +64,5 @@ pub fn binary_exists(bin: &str) -> bool {
 
 /// Ensure journal directories exist
 pub fn ensure_dirs() {
-    let user_dirs = UserDirs::new();
-    let user_dirs = user_dirs.unwrap();
-    let docs = user_dirs.document_dir().unwrap();
-    let _ = fs::create_dir_all(format!("{}/Journal/Notes", docs.to_str().unwrap()));
+    let _ = fs::create_dir_all(format!("{}/Journal/Notes", get_document_path()));
 }
